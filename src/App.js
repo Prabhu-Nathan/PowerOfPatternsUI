efaaf1b39a7fcb3d6ac677a7af232cf821ba0aaf
Prabhu Nathan <prabhuunathan24@gmail.com>
2024-01-02 00:19:12 +0530

    initial push

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { React } from "react";
+import Nav from "./components/nav/Nav";
+import Footer from "./components/footer/Footer";
+import CourseCards from "./components/coursecards/CourseCards";
+function App() {
+  return (
+    <>
+      <Nav />
+      <CourseCards />
+      <Footer />
+    </>
+
+
+  );
+}
+
+export default App;
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,8 @@
+import  './footer.css'
+export default  function Nav(){
+    return(
+         <div className="footer">
+            copyright &copy; 2023
+        </div>
+    );
+}   
