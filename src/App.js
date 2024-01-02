b1193f1bd7f434da7742236a7616be424e5dac70
prabhunathan <prabhunathan24@gmil.com>
2024-01-02 17:32:18 +0530

    added Routings and Login page.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import { React } from "react";
+import "./index.css"
 import Nav from "./components/nav/Nav";
 import Footer from "./components/footer/Footer";
 import CourseCards from "./components/coursecards/CourseCards";
+import { Route, Routes } from "react-router-dom";
+import Form from "./components/login/Form";
+import { styled } from "@mui/system";
+import { Container, CssBaseline } from "@mui/material";
+
 function App() {
+  const RootContainer = styled('div')({
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  });
+  
+  const ContentContainer = styled(Container)({
+    flexGrow: 1,
+    padding: (theme) => theme.spacing(3),
+  });
+  
+  const FooterContainer = styled('footer')({
+    padding: (theme) => theme.spacing(2),
+    marginTop: 'auto',
+  });
   return (
-    <>
-      <Nav />
-      <CourseCards />
+    <RootContainer>
+      <CssBaseline/>
+     <Nav/>
+     <ContentContainer component="main">
+      <Routes>
+          <Route exact path='/' element={<CourseCards/>} />
+          <Route path='/login' element ={<Form/>}/>
+      </Routes>
+      </ContentContainer>
       <Footer />
-    </>
-
-
-  );
+      </RootContainer>
+    );
 }
 
 export default App;
